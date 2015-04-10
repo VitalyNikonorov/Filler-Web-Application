48ae15dad0cc6ac644e2d40e6d94ece15632668c
Vitaly-Nikonorov <nik.vitaly@mail.ru>
2015-04-10 21:28:53 +0300

    clear mechanics

--- src/main/java/frontend/GameServlet.java
+++ src/main/java/frontend/GameServlet.java
@@ -22,39 +22,6 @@ import java.util.Map;
  * @author v.chibrikov
  */
 
-/*
-public class GameServlet extends HttpServlet {
-
-    private GameMechanics gameMechanics;
-    private AccountService authService;
-
-    public GameServlet(GameMechanics gameMechanics, AccountService authService) {
-        this.gameMechanics = gameMechanics;
-        this.authService = authService;
-    }
-
-    public void doGet(HttpServletRequest request,
-                      HttpServletResponse response) throws ServletException, IOException {
-    }
-
-    public void doPost(HttpServletRequest request,
-                       HttpServletResponse response) throws ServletException, IOException {
-
-        Map<String, Object> pageVariables = new HashMap<>();
-        String name = request.getParameter("name");
-        String safeName = name == null ? "NoName" : name;
-        authService.saveUserName(request.getSession().getId(), name);
-        pageVariables.put("myName", safeName);
-
-        response.getWriter().println(PageGenerator.getPage("game.html", pageVariables));
-
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-    }
-}*/
-
-
-
 public class GameServlet extends HttpServlet {
     private AccountService accountService;
     //private WebSocketService webSocketService = new WebSocketServiceImpl();
@@ -71,21 +38,6 @@ public class GameServlet extends HttpServlet {
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws ServletException, IOException {
 
-        /*Map<String, Object> pageVariables = new HashMap<>();
-        HttpSession session = request.getSession();
-        String name = "";
-        if (accountService.isExist(session.getId())) {
-            name = accountService.getSessions(session.getId()).getLogin();
-        }
-
-        String safeName = name == null ? "NoName" : name;
-        accountService.addSessions(request.getSession().getId(), accountService.getUser(name));
-        pageVariables.put("myName", safeName);
-
-        response.getWriter().println(PageGenerator.getPage("game.html", pageVariables));
-
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);*/
 
     }
 
